fd419a6ca3a18502cb57e752076a4808874618da
jtagcat <git-12dbd862@jtag.cat>
2022-11-13 23:00:13 +0200

    add TestParseAlias()

--- harg_test.go
+++ harg_test.go
@@ -8,6 +8,8 @@ import (
 )
 
 func TestNildefs(t *testing.T) {
+	t.Parallel()
+
 	defs := harg.Definitions{}
 
 	args, chokeReturn, err := defs.Parse(
@@ -21,4 +23,36 @@ func TestNildefs(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, []string{"hello", "world"}, args)
 	assert.Equal(t, []string{"choke", "return"}, chokeReturn)
+}
+
+func TestAliasParse(t *testing.T) {
+	t.Parallel()
+
+	oneKey := "one"
+
+	defs := harg.Definitions{
+		oneKey: {Type: harg.String},
+	}
+	defs.Alias("two", oneKey)
+
+	args, chokeReturn, err := defs.Parse(
+		[]string{
+			"hello",
+			"--one=one",
+			"--two", "two",
+			"world",
+		}, nil,
+	)
+
+	assert.Nil(t, err)
+	assert.Nil(t, chokeReturn)
+	assert.Equal(t, []string{"hello", "world"}, args)
+
+	sl, ok := defs[oneKey].SlString()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, []string{"one", "two"}, sl)
+
+	s, ok := defs[oneKey].String()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, "two", s)
 }
