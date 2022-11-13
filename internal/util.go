c2dae4e1312bebfcbf26d47fb4a841a8008401e4
jtagcat <git-12dbd862@jtag.cat>
2022-11-13 19:00:22 +0200

    add single key parse?

--- internal/util.go
+++ internal/util.go
@@ -14,6 +14,7 @@ func SliceToLowercaseMap(s []string) (m map[string]bool) {
 }
 
 // TODO: test
+// TODO: use LowerCaseLongKey
 func LowercaseLongMapNames[T any](m map[string]T) map[string]T {
 	for name, data := range m {
 		// short args are case sensitive, skip
@@ -29,4 +30,12 @@ func LowercaseLongMapNames[T any](m map[string]T) map[string]T {
 		}
 	}
 	return m
+}
+
+func LowercaseLongKey(key string) string {
+	if utf8.RuneCountInString(key) == 1 {
+		return key
+	}
+
+	return strings.ToLower(key)
 }
