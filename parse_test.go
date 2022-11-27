b09040a9e9dbdadb4d90195ee595e71f2ca1dbcb
jtagcat <git-12dbd862@jtag.cat>
2022-11-27 23:05:24 +0200

    format example for godoc

--- parse_test.go
+++ parse_test.go
@@ -1,6 +1,8 @@
 package harg_test
 
 import (
+	"fmt"
+	"log"
 	"strings"
 	"testing"
 
@@ -10,6 +12,50 @@ import (
 
 // see FORMAT.md for what test is
 // responsible for what part of the spec
+
+func ExampleDefinitions_Parse() {
+	kOne, kTwo, kThree := "o", "t", "three"
+	defs := harg.Definitions{
+		kOne:   {Type: harg.String},
+		kTwo:   {Type: harg.Bool},
+		kThree: {Type: harg.Duration},
+	}
+
+	osArgs := strings.Split("programName hello -to foo -o bar --three 5s --t -t -t world", " ")
+
+	args, _, err := defs.Parse(osArgs[1:], nil)
+	if err != nil {
+		log.Fatalf("parsing command-line arguments: %e", err)
+	}
+
+	fmt.Println(args) // [hello world]
+
+	sl, ok := defs[kOne].SlString() // ok: is valid and set
+	if ok {
+		fmt.Println(sl) // [foo bar]
+	}
+	s, ok := defs[kOne].String()
+	if ok {
+		fmt.Println(s) // bar
+	}
+
+	two, _ := defs[kTwo].SlBool()
+	fmt.Println(two) // [true false true true]
+
+	count, _ := defs[kTwo].Count() // how many true in a row
+	fmt.Println(count)             // 2
+
+	dur, _ := defs[kThree].Duration()
+	fmt.Println(dur) // 5s
+
+	// Output:
+	// [hello world]
+	// [foo bar]
+	// bar
+	// [true false true true]
+	// 2
+	// 5s
+}
 
 func TestParseNilDefs(t *testing.T) {
 	t.Parallel()
