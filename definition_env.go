0e7982cb6fbc087ff3e730d7bd36f715d057bf0e
jtagcat <git-12dbd862@jtag.cat>
2022-11-28 15:04:11 +0200

    rm AddPrefix
    as it conflicts with alias normalization strategy

--- definition_env.go
+++ definition_env.go
@@ -6,17 +6,6 @@ import (
 	"unicode"
 )
 
-// adds a prefix to all Definitions
-func (defs Definitions) AddPrefix(prefix string) Definitions {
-	new := make(Definitions)
-
-	for name, def := range defs {
-		new[prefix+name] = def
-	}
-
-	return new
-}
-
 func (defs Definitions) normalizeEnv() error {
 	return defs.genericNormalize(func(key string, def *Definition) (string, error) {
 		key = strings.ReplaceAll(key, " ", "_")
