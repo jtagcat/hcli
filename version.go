2d0e7774e6009139c8683fa603c4aaa56fb49141
jtagcat <git-12dbd862@jtag.cat>
2023-01-05 04:58:23 +0200

    throw version on shelf

--- version.go
+++ version.go
@@ -1,12 +1,23 @@
 package hcli
 
 var (
-	semver    string
-	gitcommit string
+	semVer    string
+	gitCommit string
+
+	VersionGiver string
 	// TODO: ...
 )
 
+// TODO: don't export gitCommit, as it then only accessible at build time
+// TODO: interface for printing version? allow calling it from elsewhere without exit?
 func version() {
 	// TODO:
 	// to json and print to stdout
 }
+
+// https://blog.alexellis.io/inject-build-time-vars-golang/
+// var GitCommit string
+// export GIT_COMMIT=$(git rev-list -1 HEAD) && \
+//   go build -ldflags "-X hcli.GitCommit=$GIT_COMMIT"
+
+// note: does not work with go run
