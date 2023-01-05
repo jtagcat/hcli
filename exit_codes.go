1c393925b781dd2980c251e166cdddc36f71bbf4
jtagcat <git-12dbd862@jtag.cat>
2023-01-05 05:37:48 +0200

    format exit codes

--- exit_codes.go
+++ exit_codes.go
@@ -32,82 +32,98 @@
 package hcli
 
 /*
- *  SYSEXITS.H -- Exit status codes for system programs.
+ *  Linux /usr/include/sysexits.h -- Exit status codes for system programs.
  *
  *	This include file attempts to categorize possible error
  *	exit statuses for system programs, notably delivermail
  *	and the Berkeley network.
  *
- *	Error numbers begin at EX__BASE to reduce the possibility of
+ *	Error numbers begin at ExitBase to reduce the possibility of
  *	clashing with other exit statuses that random programs may
- *	already return.  The meaning of the codes is approximately
- *	as follows:
- *
- *	EX_USAGE -- The command was used incorrectly, e.g., with
- *		the wrong number of arguments, a bad flag, a bad
- *		syntax in a parameter, or whatever.
- *	EX_DATAERR -- The input data was incorrect in some way.
- *		This should only be used for user's data & not
- *		system files.
- *	EX_NOINPUT -- An input file (not a system file) did not
- *		exist or was not readable.  This could also include
- *		errors like "No message" to a mailer (if it cared
- *		to catch it).
- *	EX_NOUSER -- The user specified did not exist.  This might
- *		be used for mail addresses or remote logins.
- *	EX_NOHOST -- The host specified did not exist.  This is used
- *		in mail addresses or network requests.
- *	EX_UNAVAILABLE -- A service is unavailable.  This can occur
- *		if a support program or file does not exist.  This
- *		can also be used as a catchall message when something
- *		you wanted to do doesn't work, but you don't know
- *		why.
- *	EX_SOFTWARE -- An internal software error has been detected.
- *		This should be limited to non-operating system related
- *		errors as possible.
- *	EX_OSERR -- An operating system error has been detected.
- *		This is intended to be used for such things as "cannot
- *		fork", "cannot create pipe", or the like.  It includes
- *		things like getuid returning a user that does not
- *		exist in the passwd file.
- *	EX_OSFILE -- Some system file (e.g., /etc/passwd, /etc/utmp,
- *		etc.) does not exist, cannot be opened, or has some
- *		sort of error (e.g., syntax error).
- *	EX_CANTCREAT -- A (user specified) output file cannot be
- *		created.
- *	EX_IOERR -- An error occurred while doing I/O on some file.
- *	EX_TEMPFAIL -- temporary failure, indicating something that
- *		is not really an error.  In sendmail, this means
- *		that a mailer (e.g.) could not create a connection,
- *		and the request should be reattempted later.
- *	EX_PROTOCOL -- the remote system returned something that
- *		was "not possible" during a protocol exchange.
- *	EX_NOPERM -- You did not have sufficient permission to
- *		perform the operation.  This is not intended for
- *		file system problems, which should use NOINPUT or
- *		CANTCREAT, but rather for higher level permissions.
+ *	already return.
  */
 
-// from Linux /usr/include/sysexits.h
 var (
-	ExitOk = 0 // successful termination
+	// base value for error messages
+	ExitBase = 64
+	// maximum listed value
+	ExitMax = 78
+)
 
-	ExitBase = 64 // base value for error messages
-	ExitMax  = 78 // maximum listed value
+var (
+	// successful termination
+	ExitOk = 0
 
-	ExitUsage       = 64 // command line usage error
-	ExitDataerr     = 65 // data format error
-	ExitNoinput     = 66 // cannot open input
-	ExitNouser      = 67 // addressee unknown
-	ExitNohost      = 68 // host name unknown
-	ExitUnavailable = 69 // service unavailable
-	ExitSoftware    = 70 // internal software error
-	ExitOserr       = 71 // system error (e.g., can't fork)
-	ExitOsfile      = 72 // critical OS file missing
-	ExitCantcreat   = 73 // can't create (user) output file
-	ExitIoerr       = 74 // input/output error
-	ExitTempfail    = 75 // temp failure; user is invited to retry
-	ExitProtocol    = 76 // remote error in protocol
-	ExitNoperm      = 77 // permission denied
-	ExitConfig      = 78 // configuration error
+	// command line usage error
+	//
+	// The command was used incorrectly, e.g., with the wrong number of arguments, a bad flag, a bad syntax in a parameter, or whatever.
+	ExitUsage = 64
+
+	// data format error
+	//
+	// The input data was incorrect in some way. This should only be used for user's data & not system files.
+	ExitDataerr = 65
+
+	// cannot open input
+	//
+	// An input file (not a system file) did not exist or was not readable.  This could also include errors like "No message" to a mailer (if it cared to catch it).
+	ExitNoinput = 66
+
+	// addressee unknown
+	//
+	// The user specified did not exist.  This might be used for mail addresses or remote logins.
+	ExitNouser = 67
+
+	// host name unknown
+	//
+	// The host specified did not exist.  This is used in mail addresses or network requests.
+	ExitNohost = 68
+
+	// service unavailable
+	//
+	// A service is unavailable.  This can occur if a support program or file does not exist.  This can also be used as a catchall message when something you wanted to do doesn't work, but you don't know why.
+	ExitUnavailable = 69
+
+	// internal software error
+	//
+	// An internal software error has been detected. This should be limited to non-operating system related errors as possible.
+	ExitSoftware = 70
+
+	// system error (e.g., can't fork)
+	//
+	// An operating system error has been detected. This is intended to be used for such things as "cannot fork", "cannot create pipe", or the like.  It includes things like getuid returning a user that does not exist in the passwd file.
+	ExitOserr = 71
+
+	// critical OS file missing
+	//
+	// Some system file (e.g., /etc/passwd, /etc/utmp, etc.) does not exist, cannot be opened, or has some sort of error (e.g., syntax error).
+	ExitOsfile = 72
+
+	// can't create (user) output file
+	//
+	// A (user specified) output file cannot be created.
+	ExitCantcreat = 73
+
+	// input/output error
+	//
+	// An error occurred while doing I/O on some file.
+	ExitIoerr = 74
+
+	// temp failure; user is invited to retry
+	//
+	// temporary failure, indicating something that is not really an error.  In sendmail, this means that a mailer (e.g.) could not create a connection, and the request should be reattempted later.
+	ExitTempfail = 75
+
+	// remote error in protocol
+	//
+	// the remote system returned something that was "not possible" during a protocol exchange.
+	ExitProtocol = 76
+
+	// permission denied
+	//
+	// You did not have sufficient permission to perform the operation.  This is not intended for file system problems, which should use NOINPUT or CANTCREAT, but rather for higher level permissions.
+	ExitNoperm = 77
+
+	// configuration error
+	ExitConfig = 78
 )
